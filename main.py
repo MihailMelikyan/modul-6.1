0f96dbda13929f6e360fee6dce4d190f3fdf7bd4
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-08-22 22:42:37 +0300

    испраленное задание 2.0 6.1

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 class Animal:
-    def __init__(self,name):
+    def __init__(self, name):
         self.alive = True
         self.fed = False
         self.name = name
-
 
     def eat(self, food):
         self.food = food
@@ -14,57 +13,52 @@ class Animal:
     def __str__(self):
         return f'животное : {self.name}'
 
+
 class Mammal(Animal):
-    def __init__(self,name,sound):
+    def __init__(self, name, sound):
         super().__init__(name)
         self.sound = sound
 
-    def make_sound (self):
+    def make_sound(self):
         return f'{self.name} издал звук {self.sound}'
 
 
-
 class Predator(Animal):
-    def __init__(self,name):
+    def __init__(self, name):
         super().__init__(name)
 
-
-    def hunt (self):
+    def hunt(self):
         return f'{self.name} вышел на охоту '
 
+
 class Plant:
-    def __init__(self,name):
-        self.edible = False
+    edible = False
+    def __init__(self, name):
         self.name = name
 
 
-
 class Fruit(Plant):
-    def __init__(self,name):
+    def __init__(self, name):
         super().__init__(name)
-
-
-
 
 
 class Flower(Plant):
-    def __init__(self,name):
+    def __init__(self, name):
         super().__init__(name)
 
 
 
-if __name__ == '__main__': # почему выводится ссылка а не сам обьект?
-    a1 = Predator('Волк с Уолл-Стрит')
-    a2 = Mammal('Хатико','выхлопных газов')
-    p1 = Flower('Цветик семицветик')
-    p2 = Fruit('Заводной апельсин')
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико', 'выхлопных газов')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
 
-    print(a1.name)
-    print(p1.name)
+print(a1.name)
+print(p1.name)
 
-    print(a1.alive)
-    print(a2.fed)
-    print(a1.eat(p1))
-    print(a2.eat(p2),a2.make_sound())
-    print(a1.alive)
-    print(a2.fed)
+print(a1.alive)
+print(a2.fed)
+print(a1.eat(p1))
+print(a2.eat(p2), a2.make_sound())
+print(a1.alive)
+print(a2.fed)
